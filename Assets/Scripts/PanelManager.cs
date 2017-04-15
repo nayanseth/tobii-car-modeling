23a814b06a31a66b5c31b09bc6c9b23013197670
Nayan Seth <nayantronix@gmail.com>
2017-04-14 19:06:32 -0700

    UI Canvas; Scene Setup; Gaze Point; Audi R8 Model;

---
+++ Assets/Scripts/PanelManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelManager : MonoBehaviour {
+	GameObject panel;
+
+	void Awake() {
+		panel = GameObject.Find ("Panel");
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		panel.GetComponent<Image> ().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
