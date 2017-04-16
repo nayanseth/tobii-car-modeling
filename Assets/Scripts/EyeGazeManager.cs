abbcc959894be8348699377ad980ca06cdebc08f
Nayan Seth <nayantronix@gmail.com>
2017-04-15 21:04:18 -0700

    Material Change Implemented

--- Assets/Scripts/EyeGazeManager.cs
+++ Assets/Scripts/EyeGazeManager.cs
@@ -9,17 +9,24 @@ public class EyeGazeManager : MonoBehaviour
 {
 
     GameObject rotateButton, inventoryButton, closeInventoryButton, blueButton, redButton, blackButton;
-    GazeAware rotateButtonGazeAware, inventoryButtonGazeAware, closeInventoryButtonGazeAware;
+    GazeAware rotateButtonGazeAware, inventoryButtonGazeAware, closeInventoryButtonGazeAware, redButtonGazeAware, blueButtonGazeAware, blackButtonGazeAware;
     GameObject car;
     bool rotateCar;
     bool inventoryPanel;
+	public bool modelCar;
     Animator carAnimator;
-
     VariableManager vm;
+	string implode;
+
+	GameObject temp, engine, interior, bodyFront, bodyRear;
+	GazeAware engineGazeAware, interiorGazeAware, bodyFrontGazeAware, bodyRearGazeAware;
+	Material[] originalMaterial;
+
 
     // Use this for initialization
     void Start()
     {
+		
 
         vm = GameObject.Find("Managers").GetComponent<VariableManager>();
 
@@ -46,47 +53,100 @@ public class EyeGazeManager : MonoBehaviour
         redButton = GameObject.Find("Red");
         vm.SetButton(redButton, false);
 
+		blueButtonGazeAware = blueButton.GetComponent<GazeAware> ();
+		redButtonGazeAware = redButton.GetComponent<GazeAware> ();
+		blackButtonGazeAware = blackButton.GetComponent<GazeAware> ();
 
         car = GameObject.Find("Audi R8");
         carAnimator = car.GetComponent<Animator>();
-    }
+    
+
+
+		// Car Parts
+
+		engine = GameObject.Find ("Engine");
+		engineGazeAware = engine.GetComponent<GazeAware> ();
+
+		interior = GameObject.Find ("Interior");
+		interiorGazeAware = interior.GetComponent<GazeAware> ();
+
+		bodyFront = GameObject.Find ("BodyFront");
+		bodyFrontGazeAware = bodyFront.GetComponent<GazeAware> ();
+
+		bodyRear = GameObject.Find ("BodyRear");
+		bodyRearGazeAware = bodyRear.GetComponent<GazeAware> ();
+
+		modelCar = false;
+	}
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
 
-        if (rotateButtonGazeAware.HasGazeFocus)
-        {
+
+        if (rotateButtonGazeAware.HasGazeFocus) {
             rotateCar = !rotateCar;
         }
 
-        if (rotateCar)
-        {
+        if (rotateCar) {
             car.transform.Rotate(0, 30 * Time.deltaTime, 0);
         }
 
-        if (inventoryButtonGazeAware.HasGazeFocus)
-        {
+        if (inventoryButtonGazeAware.HasGazeFocus) {
             inventoryPanel = true;
             //vm.SetPanel (true);
         }
 
-        if (closeInventoryButtonGazeAware.HasGazeFocus)
-        {
+        if (closeInventoryButtonGazeAware.HasGazeFocus) {
+			carAnimator.SetBool("Explode", false);
+			carAnimator.SetBool("Implode", true);
         }
 
-        if (inventoryPanel)
-        {
-            carAnimator.SetBool("Explode", true);
-            carAnimator.SetBool("Implode", false);
+        if (inventoryPanel) {
+			print("yes");
+			vm.SetButton (inventoryButton, false);
+			carAnimator.SetBool("Explode", true);
+			carAnimator.SetBool("Implode", false);
+
+			if (engineGazeAware.HasGazeFocus) {
+				temp = engine;
+				modelCar = true;
+				inventoryPanel = false;
+			} else if (interiorGazeAware.HasGazeFocus) {
+				temp = interior;
+				modelCar = true;
+				inventoryPanel = false;
+			} else if (bodyFrontGazeAware.HasGazeFocus || bodyRearGazeAware.HasGazeFocus) {
+				temp = bodyFront;
+				modelCar = true;
+				inventoryPanel = false;
+			}
         }
 
-    }
+		if (modelCar) {
+			vm.ModelCar (carAnimator, temp);
+			vm.SetButton (closeInventoryButton, true);
+			vm.SetButton (blueButton, true);
+			vm.SetButton (redButton, true);
+			vm.SetButton (blackButton, true);
+		}
 
-    void ExplodeCar()
-    {
-        GameObject.Find("Engine").transform.localPosition = Vector3.MoveTowards(GameObject.Find("Engine").transform.localPosition, new Vector3(0f, -0.5f, -2.52f), 1.0f * Time.deltaTime);
-    }
+		if (redButtonGazeAware.HasGazeFocus)
+			vm.SetMaterial (temp, "Red");
+		else if (blueButtonGazeAware.HasGazeFocus)
+			vm.SetMaterial (temp, "Blue");
+		else if (blackButtonGazeAware.HasGazeFocus)
+			vm.SetMaterial (temp, "Black");
+
+
+
+
+    
+	}
+
+	public void SetImplodePart(string value) {
+		implode = value;
+	}
+
 
 
 }
--- Assets/Scripts/VariableManager.cs
+++ Assets/Scripts/VariableManager.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class VariableManager : MonoBehaviour {
-	//GameObject panel;
+
+	EyeGazeManager e;
 
 	void Awake() {
-		//panel = GameObject.Find ("Panel");
-		//panel.GetComponent<Image> ().enabled = false;
+		e = GameObject.Find ("Managers").GetComponent<EyeGazeManager> ();
 	}
 
 	public void SetButton(GameObject button, bool value) {
@@ -17,4 +17,33 @@ public class VariableManager : MonoBehaviour {
 		button.GetComponentInChildren<Text>().enabled = value;
 
 	}
+
+	public void ModelCar(Animator a, GameObject carPart) {
+		if (carPart.name == "Engine") {
+			a.SetInteger ("Engine", 1);
+		}
+			
+		GameObject.Find ("Managers").GetComponent<EyeGazeManager> ().modelCar = false;
+	}
+
+	public void SetMaterial(GameObject carPart, string materialColor) {
+		Color c = Color.white;
+		if (materialColor == "Red")
+			c = Color.red;
+		else if (materialColor == "Blue")
+			c = Color.blue;
+		else if (materialColor == "Black")
+			c = Color.black;
+		
+		Renderer r = carPart.GetComponent < Renderer >();
+		Material[] originalMaterials = r.materials;
+		int length = originalMaterials.Length;
+
+		for (int i = 0; i < length; i++) {
+			r.materials [i].color = c;
+		}
+
+		e.SetImplodePart (carPart.name);
+	}
+
 }
